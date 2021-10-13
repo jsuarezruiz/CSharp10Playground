2edcaca99749bfa3177b740a04b168e000a396b4
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-10-13 20:26:24 +0200

    Constant Interpolated Strings sample

---
+++ ConstantInterpolatedStrings/ConstantInterpolatedStringsSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConstantInterpolatedStringsSample
+{
+    internal class Program
+    {
+        const string Scheme = "https"; 
+        const string Home = "home";
+
+        // Before  
+        //const string Environment = Scheme + "://localhost:5002";
+        //const string HomeUri = Environment + "/" + Home;
+
+        // Now, with C# 10 we can use constant string interpolation
+        const string Environment = $"{Scheme}://localhost:5002";
+        const string HomeUri = $"{Environment}/{Home}";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Constant Interpolated Strings {HomeUri}");
+        }
+    }
+}
